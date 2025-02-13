1c359d05b47ddc45a2b3f979ca5dd58e65825764
Pernille Holst <pernileholst94@gmail.com>
2025-02-13 15:54:30 +0100

    saved

--- JS/slideshow.js
+++ JS/slideshow.js
@@ -5,13 +5,11 @@ showSlides();
 function showSlides() {
     let i;
     let slides = document.getElementsByClassName("slidesImg");
-    for (i = 0; i < slides.length; i++)
-        {
+    for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
-    } 
+    }
     slideIndex++;
-    if (slideIndex > slides.length) 
-        {
+    if (slideIndex > slides.length) {
         slideIndex = 1
     }
 
